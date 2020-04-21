49d66a2b089aa3a8ae5ec0e05f30df548ebfd82d
Patrick Thomaz <patrick_thomaz@MacBook-Air-de-Patrick.local>
2020-04-21 20:43:19 -0300

    editorConfig eslint prettier

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 
+import routes from './routes';
+
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello World 2' });
-});
+app.use(routes);
 
 app.listen(3333, () => {
-  console.log('Server started on port 3333!')
-}); 
+  console.log('Server started on port 3333!');
+});
