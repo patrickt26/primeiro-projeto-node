3c1919828e15ea12211102e86c08042019ee7411
Patrick Thomaz <patrick_thomaz@MacBook-Air-de-Patrick.local>
2020-04-22 17:56:29 -0300

    Model & Repositories

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,24 +1,18 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
+import { startOfHour, parseISO } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/Appointmentsrepository';
 
 const appointmentsRouter = Router();
-
-interface Appointment {
-  id: string;
-  provider: string;
-  date: Date;
-}
-
-const appointments: Appointment[] = [];
+const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.post('/', (req, res) => {
   const { provider, date } = req.body;
 
   const parsedDate = startOfHour(parseISO(date));
 
-  const findAppointmentInSameDate = appointments.find(appointment =>
-    isEqual(parsedDate, appointment.date),
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(
+    parsedDate,
   );
 
   if (findAppointmentInSameDate) {
@@ -27,19 +21,13 @@ appointmentsRouter.post('/', (req, res) => {
       .json({ error: 'This appointment is already booked' });
   }
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date: parsedDate,
-  };
-
-  appointments.push(appointment);
+  const appointment = appointmentsRepository.create(provider, parsedDate);
 
   return res.json(appointment);
 });
 
-appointmentsRouter.get('/', (req, res) => {
-  return res.json(appointments);
-});
+// appointmentsRouter.get('/', (req, res) => {
+//   return res.json(appointments);
+// });
 
 export default appointmentsRouter;
